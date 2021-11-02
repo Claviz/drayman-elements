a1512120b5a15a28b3e85b23b90c5e27908e2e66
Jans Ivans Jevdokimovs <jans.ivans@gmail.com>
2021-11-02 13:07:41 +0200

    Rollback playground example.

--- playground/src/components/home.tsx
+++ playground/src/components/home.tsx
@@ -1,50 +1,19 @@
-
-export const component: DraymanComponent = async ({ forceUpdate, Browser }) => {
-
-    let startDate;
-    let endDate;
-    const allowedDates = [];
-    for (let i = 10; i < 25; i++) {
-        allowedDates.push(`2021-07-${i}`);
-    }
-    let minEndDate;
-    let maxStartDate;
+export const component: DraymanComponent = async ({ forceUpdate }) => {
+    let counter = 0;
 
     return () => {
+
         return (
             <div>
-                <div style={{ display: 'flex', gap: '5px' }}>
-                    <drayman-datepicker
-                        value={startDate}
-                        ref="startDate"
-                        allowedDates={allowedDates}
-                        maxDate={maxStartDate}
-                        label="Start date"
-                        onValueChange={async ({ value }) => {
-                            startDate = value;
-                            minEndDate = startDate;
-                            await forceUpdate();
-                            await Browser.moveCalendarTo({ date: startDate }, ['endDate']);
-                        }}
-                    />
-                    <drayman-datepicker
-                        value={endDate}
-                        ref="endDate"
-                        allowedDates={allowedDates}
-                        minDate={minEndDate}
-                        label="End date"
-                        onValueChange={async ({ value }) => {
-                            endDate = value;
-                            maxStartDate = endDate;
-                            await forceUpdate();
-                            await Browser.moveCalendarTo({ date: endDate }, ['startDate']);
-                        }}
-                    />
-                </div>
-                {
-                    JSON.stringify({ startDate, endDate })
-                }
+                <drayman-button
+                    label="Click me"
+                    onClick={async () => {
+                        counter++;
+                        await forceUpdate();
+                    }}
+                />
+                <p>Button was clicked {counter} times</p>
             </div>
-        )
+        );
     }
 }
