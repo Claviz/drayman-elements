b7f51a2dc841397f1723e3593141a6cd9008b13e
Jans Ivans Jevdokimovs <jans.ivans@gmail.com>
2022-02-08 16:21:05 +0200

    Fix nebula types

--- projects/nebula/src/app/models/nebula-options.ts
+++ projects/nebula/src/app/models/nebula-options.ts
@@ -2,19 +2,19 @@ export interface DraymanNebula {
     /**
      * Executed when user makes selections.
      */
-    onSelections?: ElementEvent<{ value: { selections: any, method: 'selectHyperCubeValues' | 'rangeSelectHyperCubeValues' } }>;
+    onSelections?: ElementEvent<{ selections: any, method: 'selectHyperCubeValues' | 'rangeSelectHyperCubeValues' }>;
     /**
      * Executed when viz method must be called.
      */
-    onVizMethod?: ElementEvent<{ value: { args: any, name: 'getEffectiveProperties' | 'getHyperCubeReducedData' | 'getHyperCubeData' } }>;
+    onVizMethod?: ElementEvent<{ args: any, name: 'getEffectiveProperties' | 'getHyperCubeReducedData' | 'getHyperCubeData' }>;
     /**
      * Executed when measure needs to be returned
      */
-    onGetMeasure?: ElementEvent<{ value: { measureId: string; } }>;
+    onGetMeasure?: ElementEvent<{ measureId: string; }>;
     /**
      * Executed when object needs to be returned
      */
-    onGetObject?: ElementEvent<{ value: { objectId: string; } }>;
+    onGetObject?: ElementEvent<{ objectId: string; }>;
     /**
      * Initial qLayout of the object to render.
      */
